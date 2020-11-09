df8650984130a1ae07a43a2d7f3e8e87f1fb5e8e
MDWojcieszak <wojcieszak.4@gmail.com>
2020-11-09 16:42:03 +0100

    upload end point

--- app.js
+++ app.js
@@ -7,24 +7,33 @@ require("dotenv/config");
 //Middlewares
 app.use(express.json());
 app.use(cors());
-
+app.use("/public", express.static("public"));
 //Import routes
 const printersRoute = require("./routes/printers");
 const filamentRoute = require("./routes/filaments");
 const printerSettingsRoute = require("./routes/printerSettings");
 const authorizationRoute = require("./routes/authorization");
+const uploadRoute = require("./routes/upload");
 
 app.use("/printers", printersRoute);
 app.use("/filaments", filamentRoute);
 app.use("/printersSettings", printerSettingsRoute);
 app.use("/user", authorizationRoute);
+app.use("/upload", uploadRoute);
 //Routes
 app.get("/", (req, res) => {
   res.send("Welcome in rest-API!");
 });
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
-  console.log("Connected to DB")
-);
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log(err);
+  });
 //Api port
 app.listen(3000);
---
+++ routes/upload.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+var fs = require("fs");
+const multer = require("multer");
+
+const str = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/img");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  },
+});
+const file_filter = (req, file, cb) => {
+  if (file.mimetype === "image/png" || file.mimetype === "image/jpeg")
+    cb(null, true);
+  else cb(null, false);
+};
+const upload = multer({
+  storage: str,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: file_filter,
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  console.log(
+    "New image is uploaded, size: " + req.file.size / 1024 / 1024 + " MB"
+  );
+  res.status(200).send({
+    fileName: req.file.filename,
+    path: "/public/img/",
+  });
+});
+
+router.patch("/:imagePath", upload.single("image"), (req, res) => {
+  try {
+    console.log(req.params.imagePath);
+    fs.unlinkSync("public/img/" + req.params.imagePath);
+  } catch (err) {
+    res.status(500).send("Error in deleting image");
+  }
+  console.log(
+    "Old image is deleted\nNew image is uploaded, size: " +
+      req.file.size / 1024 / 1024 +
+      " MB"
+  );
+  res.status(200).send({
+    fileName: req.file.filename,
+    path: "/public/img/",
+  });
+});
+
+router.delete("/:imagePath", (req, res) => {
+  try {
+    console.log(req.params.imagePath);
+    fs.unlinkSync("public/img/" + req.params.imagePath);
+    res.status(201).send("Succesfully deleted an image");
+  } catch (err) {
+    res.status(500).send("Error in deleting image");
+  }
+});
+
+module.exports = router;
